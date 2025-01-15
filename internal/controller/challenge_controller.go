f91b151b908bf6cb59c5280eed2e70719c2215f8
S0okJu <65109465+D7MeKz@users.noreply.github.com>
2025-01-15 15:25:29 +0900

    [Feat] Make rbac about deployment and service

--- internal/controller/challenge_controller.go
+++ internal/controller/challenge_controller.go
@@ -51,6 +51,10 @@ type ChallengeReconciler struct {
 // +kubebuilder:rbac:groups=apps.hexactf.io,resources=challenges,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps.hexactf.io,resources=challenges/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps.hexactf.io,resources=challenges/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
