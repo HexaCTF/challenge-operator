6e795e880adb438653ee684341ad83675a88055c
김다경(Da Gyeong Kim) <65109465+S0okJu@users.noreply.github.com>
2025-08-08 16:14:30 +0900

    Release v1.0.0 (#33)
    * Feat: Remove kafka - Remove kafka module in internal
    - Remove sending message to queue logic in controller logic
    
    Closes #23
    
    * Feat: Remove v1alpha
    
    - Delete v1alpha api, controller code to avoid conflict.
    
    Closes #24
    
    * Feat: Remove promethues custom matrix logic
    
    - Delete prometheus custom matrix logic in controller module.
    
    Closes #27
    
    * Revert "Feat: Remove promethues custom matrix logic"
    
    * Feat: Add test codes (#32)
    
    * feat: Add handler test code(#31)
    
    * feat: Add handler test code(#31)
    
    * feat: Add package testify/mock(#31)
    
    * feat: Add integration test code(#31)

--- internal/controller/v2/challenge_definition_test.go
+++ internal/controller/v2/challenge_definition_test.go
@@ -1,1 +1,546 @@
 package controller
+
+import (
+	"context"
+	"fmt"
+
+	hexactfproj "github.com/hexactf/challenge-operator/api/v2alpha1"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("Challenge Definition", func() {
+	var (
+		client     client.Client
+		reconciler *ChallengeReconciler
+		ctx        context.Context
+	)
+
+	BeforeEach(func() {
+		client = CreateFakeClient()
+		reconciler = &ChallengeReconciler{
+			Client: client,
+			Scheme: client.Scheme(),
+		}
+		ctx = context.Background()
+	})
+
+	Describe("LoadChallengeDefinition", func() {
+		Context("when challenge definition exists", func() {
+			It("should load challenge definition successfully", func() {
+				// Given
+				challenge := NewChallengeBuilder().
+					WithName("test-challenge").
+					WithNamespace("default").
+					WithPodName("test-pod").
+					Build()
+
+				definition := &hexactfproj.ChallengeDefinition{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-definition",
+						Namespace: "default",
+					},
+					Spec: hexactfproj.ChallengeDefinitionSpec{
+						Resource: hexactfproj.Resource{
+							Pod: &hexactfproj.PodConfig{
+								Containers: []hexactfproj.ContainerConfig{
+									{
+										Name:  "test-container",
+										Image: "nginx:latest",
+									},
+								},
+							},
+							Service: &corev1.Service{
+								ObjectMeta: metav1.ObjectMeta{
+									Name: "test-service",
+								},
+								Spec: corev1.ServiceSpec{
+									Type: corev1.ServiceTypeNodePort,
+									Ports: []corev1.ServicePort{
+										{
+											Port:     80,
+											NodePort: 30080,
+										},
+									},
+								},
+							},
+						},
+					},
+				}
+
+				// Create definition in client
+				err := client.Create(ctx, definition)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Create challenge in client
+				err = client.Create(ctx, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Get the challenge from client to ensure it exists
+				err = client.Get(ctx, types.NamespacedName{
+					Name:      challenge.Name,
+					Namespace: challenge.Namespace,
+				}, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				// When
+				err = reconciler.loadChallengeDefinition(ctx, ctrl.Request{}, challenge)
+
+				// Then
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify labels were set correctly
+				expectedLabels := map[string]string{
+					"apps.hexactf.io/challengeId":   "test-challenge-id",
+					"apps.hexactf.io/userId":        "test-user-id",
+					"apps.hexactf.io/createdBy":     "challenge-operator",
+					"apps.hexactf.io/challengeName": "challenge-test-challenge-id-test-user-id",
+					"apps.hexactf.io/podName":       "challenge-test-challenge-id-test-user-id-pod",
+					"apps.hexactf.io/svcName":       "challenge-test-challenge-id-test-user-id-svc",
+				}
+
+				for key, expectedValue := range expectedLabels {
+					actualValue, exists := challenge.Labels[key]
+					Expect(exists).To(BeTrue(), "Expected label %s to exist", key)
+					Expect(actualValue).To(Equal(expectedValue), "Expected label %s to be %s, but got %s", key, expectedValue, actualValue)
+				}
+			})
+		})
+
+		Context("when challenge definition does not exist", func() {
+			It("should return an error", func() {
+				// Given
+				challenge := NewChallengeBuilder().
+					WithName("test-challenge").
+					WithNamespace("default").
+					Build()
+
+				// Create challenge in client but not definition
+				err := client.Create(ctx, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				// When
+				err = reconciler.loadChallengeDefinition(ctx, ctrl.Request{}, challenge)
+
+				// Then
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("LoadPod", func() {
+		It("should load pod successfully", func() {
+			// Given
+			challenge := NewChallengeBuilder().
+				WithName("test-challenge").
+				WithNamespace("default").
+				WithPodName("test-pod").
+				Build()
+
+			pod := &corev1.Pod{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-pod",
+					Namespace: "default",
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "test-container",
+							Image: "nginx:latest",
+						},
+					},
+				},
+			}
+
+			// When
+			err := reconciler.loadPod(ctx, challenge, pod)
+
+			// Then
+			Expect(err).NotTo(HaveOccurred())
+
+			// Verify pod was created
+			createdPod := &corev1.Pod{}
+			err = client.Get(ctx, types.NamespacedName{
+				Name:      "test-pod",
+				Namespace: "default",
+			}, createdPod)
+			Expect(err).NotTo(HaveOccurred())
+
+			// Verify status was set to pending
+			Expect(challenge.Status.CurrentStatus.Status).To(Equal("Pending"))
+		})
+	})
+
+	Describe("LoadService", func() {
+		Context("with NodePort service", func() {
+			It("should load service successfully", func() {
+				// Given
+				challenge := NewChallengeBuilder().
+					WithName("test-challenge").
+					WithNamespace("default").
+					Build()
+
+				// Set the required labels for service creation
+				challenge.Labels["apps.hexactf.io/svcName"] = "test-service"
+
+				// Create challenge in client
+				err := client.Create(ctx, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Get the challenge from client to ensure it exists
+				err = client.Get(ctx, types.NamespacedName{
+					Name:      challenge.Name,
+					Namespace: challenge.Namespace,
+				}, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				service := &corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-service",
+						Namespace: "default",
+					},
+					Spec: corev1.ServiceSpec{
+						Type: corev1.ServiceTypeNodePort,
+						Ports: []corev1.ServicePort{
+							{
+								Port:     80,
+								NodePort: 30080,
+							},
+						},
+					},
+				}
+
+				// When
+				err = reconciler.loadService(ctx, challenge, service)
+
+				// Then
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify service was created
+				createdService := &corev1.Service{}
+				err = client.Get(ctx, types.NamespacedName{
+					Name:      "test-service",
+					Namespace: "default",
+				}, createdService)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify endpoint was set
+				Expect(challenge.Status.Endpoint).To(Equal(30080))
+			})
+		})
+
+		Context("with ClusterIP service", func() {
+			It("should load service successfully without setting endpoint", func() {
+				// Given
+				challenge := NewChallengeBuilder().
+					WithName("test-challenge").
+					WithNamespace("default").
+					Build()
+
+				// Set the required labels for service creation
+				challenge.Labels["apps.hexactf.io/svcName"] = "test-service"
+
+				// Create challenge in client
+				err := client.Create(ctx, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Get the challenge from client to ensure it exists
+				err = client.Get(ctx, types.NamespacedName{
+					Name:      challenge.Name,
+					Namespace: challenge.Namespace,
+				}, challenge)
+				Expect(err).NotTo(HaveOccurred())
+
+				service := &corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test-service",
+						Namespace: "default",
+					},
+					Spec: corev1.ServiceSpec{
+						Type: corev1.ServiceTypeClusterIP,
+						Ports: []corev1.ServicePort{
+							{
+								Port: 80,
+							},
+						},
+					},
+				}
+
+				// When
+				err = reconciler.loadService(ctx, challenge, service)
+
+				// Then
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify service was created
+				createdService := &corev1.Service{}
+				err = client.Get(ctx, types.NamespacedName{
+					Name:      "test-service",
+					Namespace: "default",
+				}, createdService)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify endpoint was not set for ClusterIP
+				Expect(challenge.Status.Endpoint).To(Equal(0))
+			})
+		})
+	})
+
+	Describe("GetChallengeDefinition", func() {
+		It("should get challenge definition successfully", func() {
+			// Given
+			challenge := NewChallengeBuilder().
+				WithName("test-challenge").
+				WithNamespace("default").
+				Build()
+
+			definition := &hexactfproj.ChallengeDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-definition",
+					Namespace: "default",
+				},
+				Spec: hexactfproj.ChallengeDefinitionSpec{
+					Resource: hexactfproj.Resource{
+						Pod: &hexactfproj.PodConfig{
+							Containers: []hexactfproj.ContainerConfig{
+								{
+									Name:  "test-container",
+									Image: "nginx:latest",
+								},
+							},
+						},
+					},
+				},
+			}
+
+			// Create definition in client
+			err := client.Create(ctx, definition)
+			Expect(err).NotTo(HaveOccurred())
+
+			// When
+			retrievedDefinition, err := reconciler.getChallengeDefinition(ctx, challenge)
+
+			// Then
+			Expect(err).NotTo(HaveOccurred())
+			Expect(retrievedDefinition).NotTo(BeNil())
+			Expect(retrievedDefinition.Name).To(Equal("test-definition"))
+		})
+	})
+})
+
+var _ = Describe("Challenge Definition Performance", func() {
+	var (
+		client     client.Client
+		reconciler *ChallengeReconciler
+		ctx        context.Context
+	)
+
+	BeforeEach(func() {
+		client = CreateFakeClient()
+		reconciler = &ChallengeReconciler{
+			Client: client,
+			Scheme: client.Scheme(),
+		}
+		ctx = context.Background()
+	})
+
+	It("should load challenge definition efficiently", func() {
+		// Create a fresh challenge for each iteration with unique name
+		challengeName := fmt.Sprintf("benchmark-challenge-%d", GinkgoParallelProcess())
+		definitionName := fmt.Sprintf("benchmark-definition-%d", GinkgoParallelProcess())
+		challengeId := fmt.Sprintf("benchmark-challenge-id-%d", GinkgoParallelProcess())
+		userId := fmt.Sprintf("benchmark-user-id-%d", GinkgoParallelProcess())
+
+		challenge := NewChallengeBuilder().
+			WithName(challengeName).
+			WithNamespace("default").
+			Build()
+
+		// Set unique challengeId and userId to ensure unique pod/service names
+		challenge.Labels["apps.hexactf.io/challengeId"] = challengeId
+		challenge.Labels["apps.hexactf.io/userId"] = userId
+
+		// Create a fresh definition for each iteration
+		definition := &hexactfproj.ChallengeDefinition{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      definitionName,
+				Namespace: "default",
+			},
+			Spec: hexactfproj.ChallengeDefinitionSpec{
+				Resource: hexactfproj.Resource{
+					Pod: &hexactfproj.PodConfig{
+						Containers: []hexactfproj.ContainerConfig{
+							{
+								Name:  "benchmark-container",
+								Image: "nginx:latest",
+							},
+						},
+					},
+					Service: &corev1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "benchmark-service",
+						},
+						Spec: corev1.ServiceSpec{
+							Type: corev1.ServiceTypeNodePort,
+							Ports: []corev1.ServicePort{
+								{
+									Port:     80,
+									NodePort: 30080,
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		// Create definition in client
+		err := client.Create(ctx, definition)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Update the challenge to reference the correct definition
+		challenge.Spec.Definition = definitionName
+
+		err = client.Create(ctx, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Get the challenge from client to ensure it exists
+		err = client.Get(ctx, types.NamespacedName{
+			Name:      challenge.Name,
+			Namespace: challenge.Namespace,
+		}, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = reconciler.loadChallengeDefinition(ctx, ctrl.Request{}, challenge)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should load pod efficiently", func() {
+		// Create a fresh challenge for each iteration with unique name
+		challengeName := fmt.Sprintf("benchmark-challenge-%d", GinkgoParallelProcess())
+		podName := fmt.Sprintf("benchmark-pod-%d", GinkgoParallelProcess())
+		challenge := NewChallengeBuilder().
+			WithName(challengeName).
+			WithNamespace("default").
+			WithPodName(podName).
+			Build()
+
+		// Create challenge in client
+		err := client.Create(ctx, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Get the challenge from client to ensure it exists
+		err = client.Get(ctx, types.NamespacedName{
+			Name:      challenge.Name,
+			Namespace: challenge.Namespace,
+		}, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		pod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      podName,
+				Namespace: "default",
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{
+					{
+						Name:  "benchmark-container",
+						Image: "nginx:latest",
+					},
+				},
+			},
+		}
+
+		err = reconciler.loadPod(ctx, challenge, pod)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should load service efficiently", func() {
+		// Create a fresh challenge for each iteration with unique name
+		challengeName := fmt.Sprintf("benchmark-challenge-%d", GinkgoParallelProcess())
+		challenge := NewChallengeBuilder().
+			WithName(challengeName).
+			WithNamespace("default").
+			Build()
+
+		// Set the required labels for service creation
+		serviceName := fmt.Sprintf("benchmark-service-%d", GinkgoParallelProcess())
+		challenge.Labels["apps.hexactf.io/svcName"] = serviceName
+
+		// Create challenge in client
+		err := client.Create(ctx, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		// Get the challenge from client to ensure it exists
+		err = client.Get(ctx, types.NamespacedName{
+			Name:      challenge.Name,
+			Namespace: challenge.Namespace,
+		}, challenge)
+		Expect(err).NotTo(HaveOccurred())
+
+		service := &corev1.Service{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      serviceName,
+				Namespace: "default",
+			},
+			Spec: corev1.ServiceSpec{
+				Type: corev1.ServiceTypeNodePort,
+				Ports: []corev1.ServicePort{
+					{
+						Port:     80,
+						NodePort: 30080,
+					},
+				},
+			},
+		}
+
+		err = reconciler.loadService(ctx, challenge, service)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should get challenge definition efficiently", func() {
+		// Create a fresh challenge for each iteration with unique name
+		challengeName := fmt.Sprintf("benchmark-challenge-%d", GinkgoParallelProcess())
+		definitionName := fmt.Sprintf("benchmark-definition-%d", GinkgoParallelProcess())
+
+		challenge := NewChallengeBuilder().
+			WithName(challengeName).
+			WithNamespace("default").
+			Build()
+
+		// Update the challenge to reference the correct definition
+		challenge.Spec.Definition = definitionName
+
+		// Create a fresh definition for each iteration
+		definition := &hexactfproj.ChallengeDefinition{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      definitionName,
+				Namespace: "default",
+			},
+			Spec: hexactfproj.ChallengeDefinitionSpec{
+				Resource: hexactfproj.Resource{
+					Pod: &hexactfproj.PodConfig{
+						Containers: []hexactfproj.ContainerConfig{
+							{
+								Name:  "benchmark-container",
+								Image: "nginx:latest",
+							},
+						},
+					},
+				},
+			},
+		}
+
+		// Create definition in client
+		err := client.Create(ctx, definition)
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = reconciler.getChallengeDefinition(ctx, challenge)
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
--- internal/controller/v2/suite_test.go
+++ internal/controller/v2/suite_test.go
@@ -18,18 +18,15 @@ package controller
 
 import (
 	"context"
-	"fmt"
-	"path/filepath"
-	"runtime"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -40,17 +37,9 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient client.Client
-var testEnv *envtest.Environment
 var ctx context.Context
 var cancel context.CancelFunc
-
-func TestControllers(t *testing.T) {
-	RegisterFailHandler(Fail)
-
-	RunSpecs(t, "Controller Suite")
-}
 
 var _ = BeforeSuite(func() {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
@@ -58,39 +47,25 @@ var _ = BeforeSuite(func() {
 	ctx, cancel = context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: true,
+	// Use fake client instead of envtest for unit tests
+	scheme := runtime.NewScheme()
+	_ = appsv2alpha1.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
 
-		// The BinaryAssetsDirectory is only required if you want to run the tests directly
-		// without call the makefile target test. If not informed it will look for the
-		// default path defined in controller-runtime which is /usr/local/kubebuilder/.
-		// Note that you must have the required binaries setup under the bin directory to perform
-		// the tests directly. When we run make test it will be setup and used automatically.
-		BinaryAssetsDirectory: filepath.Join("..", "..", "bin", "k8s",
-			fmt.Sprintf("1.31.0-%s-%s", runtime.GOOS, runtime.GOARCH)),
-	}
+	k8sClient = fake.NewClientBuilder().
+		WithScheme(scheme).
+		Build()
 
-	var err error
-	// cfg is defined in this file globally.
-	cfg, err = testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
-
-	err = appsv2alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	// +kubebuilder:scaffold:scheme
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
 	Expect(k8sClient).NotTo(BeNil())
-
 })
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
 	cancel()
-	err := testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
 })
+
+// TestControllers는 모든 테스트를 실행하는 메인 함수
+func TestControllers(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controller Suite")
+}
